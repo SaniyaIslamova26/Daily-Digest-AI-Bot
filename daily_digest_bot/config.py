b65edd14caaeb3e483db7bf3551627934ed0d17f
SaniyaIslamova26 <saniya.islamova.02@mail.ru>
2025-11-21 00:33:03 +0300

    Delete daily_digest_bot directory

--- daily_digest_bot/config.py
+++
@@ -1,3 +0,0 @@
-# Конфигурационный файл проекта
-BOT_TOKEN = "8526104408:AAFBlz72LIuAwQja4YvW_7UU2WaSWD-xKHk"  # Токен Telegram-бота
-ADMIN_ID = 695384856                                          # ID администратора системы
